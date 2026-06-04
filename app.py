441d2e24dcdf40553338be2294697b2a3d45095d
Ludo110 <ludovic.cayre@yahoo.fr>
2026-06-04 14:45:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,14 @@ st.markdown(f"<div class='rect-style' style='padding:15px; text-align:center; ma
 abritees = [p for p in plages if (True if vitesse < 10 else (p["Min"] <= angle <= p["Max"] if p["Min"] <= p["Max"] else (angle >= p["Min"] or angle <= p["Max"])))]
 exposees = [p for p in plages if p not in abritees]
 
-st.markdown("<h3 style='color:white; text-align:center;'>A l'abri</h3>", unsafe_allow_html=True)
+# Affichage du titre avec le palmier
+st.markdown("<br>", unsafe_allow_html=True)
+col1, col2 = st.columns([0.05, 0.95])
+with col1:
+    st.image("palmier.png", width=40)
+with col2:
+    st.markdown("<h3 style='color:white; margin-top:5px;'>A l'abri</h3>", unsafe_allow_html=True)
+
 html_a = "<div class='centrage-fixe'>"
 for p in abritees:
     q = urllib.parse.quote(p['Nom'] + " " + p['Ville'])
