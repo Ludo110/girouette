509b74f0f2801d9fecbd38a98f9b46037fbcb360
Ludo110 <ludovic.cayre@yahoo.fr>
2026-09-10 11:47:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 
-url = "https://www.infoclimat.fr/public-api/static/json/?id=000YV&auth=Tldx1OehbMsR6xzpQDzArHPJkGeBZX9Gb8dF0Qd3pqaUpart2w&format=json"
-data = requests.get(url).json()
-print(data.get("temperature"))
+url = "https://www.infoclimat.fr/public-api/gfs/json?id=000YV&auth=Tldx1OehbMsR6xzpQDzArHPJkGeBZX9Gb8dF0Qd3pqaUpart2w&format=json"
+resp = requests.get(url)
+print("Code statut :", resp.status_code)
+print("Contenu brut :", resp.text[:500])
